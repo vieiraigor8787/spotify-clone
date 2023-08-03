07fbd0ab6470c669dad84f662f3c4c1797d65d3f
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-03 17:12:04 -0300

    creating a new action

--- types.ts
+++ types.ts
@@ -10,6 +10,15 @@ export interface UserDetails {
   payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type]
 }
 
+export interface Song {
+  id: string
+  user_id: string
+  author: string
+  title: string
+  song_path: string
+  image_path: string
+}
+
 export interface Product {
   id: string
   active?: boolean
