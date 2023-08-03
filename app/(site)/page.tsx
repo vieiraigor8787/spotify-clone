6bfedac7e1c674b09506e181355492b54befa8ce
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-03 17:38:57 -0300

    little adjustment

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/Header'
 import ListItem from '@/components/ListItem'
-import getSongs from '@/actions/getSongs'
 import PageContent from '@/components/PageContent'
+import getSongs from '@/actions/getSongs'
 
 export const revalidate = 0
 
@@ -29,6 +29,9 @@ export default async function Home() {
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Músicas novas</h1>
+        </div>
+        <div>
+          <PageContent songs={songs} />
         </div>
       </div>
     </div>
