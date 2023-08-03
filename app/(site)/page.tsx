ea2531cdedf2ce639449494215aee707edc93d80
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-03 17:18:46 -0300

    create page content

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
 import Header from '@/components/Header'
 import ListItem from '@/components/ListItem'
+import getSongs from '@/actions/getSongs'
+import PageContent from '@/components/PageContent'
 
-export default function Home() {
+export const revalidate = 0
+
+export default async function Home() {
+  const songs = await getSongs()
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -24,7 +30,6 @@ export default function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Músicas novas</h1>
         </div>
-        <div>Lista de músicas</div>
       </div>
     </div>
   )
---
+++ components/PageContent.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Song } from '@/types'
+
+interface PageCotentProps {
+  songs: Song[]
+}
+
+export default function PageContent({ songs }: PageCotentProps) {
+  if (songs.length !== 0) {
+    return (
+      <div className="mt-4 text-neutral-400">Nenhuma música disponível</div>
+    )
+  }
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4"></div>
+  )
+}
