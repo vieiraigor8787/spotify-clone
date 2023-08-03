900f1fd1013c0df4ceaf50d6b64df33806ae562b
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-03 11:36:11 -0300

    authmodal adjustments

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,16 +1,49 @@
 'use cliente'
 
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from '@supabase/auth-helpers-react'
+
+import useAuthModal from '@/hooks/useAuthModal'
+
 import Modal from './Modal'
 
 export default function AuthModal() {
+  const supabaseClient = useSupabaseClient()
+  const { onClose, isOpen } = useAuthModal()
+
+  const onChange = (open: boolean) => {
+    if (!isOpen) {
+      onClose()
+    }
+  }
+
   return (
     <Modal
       title="Bem vindo de volta"
       description="faça o login novamente"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
-      Bem vindo de volta
+      <Auth
+        theme="dark"
+        providers={['github', 'google']}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e',
+              },
+            },
+          },
+        }}
+      />
     </Modal>
   )
 }
