5046b3fae89cbd032f646fa7cc17c54baa49a999
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-04 15:42:55 -0300

    constructing liked page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,6 +10,8 @@ interface SearchProps {
   }
 }
 
+export const revalidate = 0
+
 export default async function page({ searchParams }: SearchProps) {
   const songs = await getSongsByTitle(searchParams.title)
 
