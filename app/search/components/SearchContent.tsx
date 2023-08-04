a0826083fafe8ddc47a3eef57e8ab050349413e2
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-04 13:09:12 -0300

    configurating search content

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Song } from '@/types'
+
+interface SearcContentProps {
+  songs: Song[]
+}
+
+export default function SearchContent({ songs }: SearcContentProps) {
+  if (songs.length === 0) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        Nenhuma música encontrada
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
