2e6b307cc520a7578393420b1a707a526a739527
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-04 13:02:53 -0300

    configurating search input

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -15,7 +15,7 @@ export default async function page({ searchParams }: SearchProps) {
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
-          <h1 className="text-white text-3xl font-semibold">Search</h1>
+          <h1 className="text-white text-3xl font-semibold">Buscar</h1>
         </div>
 
         <SearchInput />
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,36 @@
 'use client'
 
+import qs from 'query-string'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import useDebounce from '@/hooks/useDebounce'
+
+import Input from './Input'
+
 export default function SearchInput() {
-  return <div>SearchInput</div>
+  const router = useRouter()
+  const [value, setValue] = useState<string>('')
+  const debouncedValue = useDebounce<string>(value, 500)
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    }
+
+    const url = qs.stringifyUrl({
+      url: '/search',
+      query: query,
+    })
+
+    router.push(url)
+  }, [debouncedValue, router])
+
+  return (
+    <Input
+      placeholder="O que você quer ouvir agora?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  )
 }
