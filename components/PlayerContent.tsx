b35a4a208cc9a8c95c47862cd6e0f7d9d25325a2
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-07 15:52:11 -0300

    set slide volume control and config prev/next btn

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { useState } from 'react'
 import { BsPauseFill, BsPlayFill } from 'react-icons/Bs'
+import { AiFillStepBackward, AiFillStepForward } from 'react-icons/Ai'
+import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/Hi2'
 
+import usePlayer from '@/hooks/usePlayer'
 import MediaItem from './MediaItem'
 import LikeButton from './LikeButton'
+import Slider from './Slider'
 
 import { Song } from '@/types'
 
@@ -13,7 +18,42 @@ interface PlayerContentProps {
 }
 
 export default function PlayerContent({ songUrl, song }: PlayerContentProps) {
-  const Icon = true ? BsPauseFill : BsPlayFill
+  const player = usePlayer()
+  const [volume, setVolume] = useState(1)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId)
+    const nextSong = player.ids[currentIndex + 1]
+
+    if (!nextSong) {
+      return player.setId(player.ids[0])
+    }
+
+    player.setId(nextSong)
+  }
+
+  const onPlayPrev = () => {
+    if (player.ids.length === 0) {
+      return
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId)
+    const previousSong = player.ids[currentIndex - 1]
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1])
+    }
+
+    player.setId(previousSong)
+  }
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -30,6 +70,34 @@ export default function PlayerContent({ songUrl, song }: PlayerContentProps) {
           onClick={() => {}}
         >
           <Icon size={30} className="text-black" />
+        </div>
+      </div>
+
+      <div className="hidden h-full md:flex items-center justify-center w-full max-w-[722px] gap-x-6">
+        <AiFillStepBackward
+          size={30}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+          onClick={onPlayPrev}
+        />
+
+        <div
+          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+          onClick={() => {}}
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+
+        <AiFillStepForward
+          onClick={onPlayNext}
+          size={30}
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
+        />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon onClick={() => {}} className="cursor-pointer" size={34} />
+          <Slider />
         </div>
       </div>
     </div>
