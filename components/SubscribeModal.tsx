cd41ba31abb8a9f185ba32baf1e04e4b7407c3a3
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-08 15:44:04 -0300

    new cases for subscription threating

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,5 +1,12 @@
 'use client'
 
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+
+import { postData } from '@/libs/helpers'
+import { getStripe } from '@/libs/stripeClient'
+import { useUser } from '@/hooks/useUser'
+
 import Button from './Button'
 import Modal from './Modal'
 
@@ -20,6 +27,37 @@ const formatPrice = (price: Price) => {
 }
 
 export default function SubscribeModal({ products }: SubscribeModalProps) {
+  const { user, isLoading, subscription } = useUser()
+  const [priceIdLoading, setPriceIdLoading] = useState<string>()
+
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id)
+
+    if (!user) {
+      setPriceIdLoading(undefined)
+      return toast.error('Necessário fazer o login')
+    }
+
+    if (subscription) {
+      setPriceIdLoading(undefined)
+      return toast('Você já é Premium')
+    }
+
+    try {
+      const { sessionId } = await postData({
+        url: '/api/create-checkout-session',
+        data: { price },
+      })
+
+      const stripe = await getStripe()
+      stripe?.redirectToCheckout({ sessionId })
+    } catch (error) {
+      toast.error((error as Error)?.message)
+    } finally {
+      setPriceIdLoading(undefined)
+    }
+  }
+
   let content = <div className="text-center">Nenhum produto disponível.</div>
 
   if (products.length) {
@@ -30,8 +68,13 @@ export default function SubscribeModal({ products }: SubscribeModalProps) {
             return <div key={product.id}>Nenhum preço disponivel</div>
           }
           return product.prices.map((price) => (
-            <Button key={price.id} onClick={() => {}} disabled>
-              {`Seja premium por apenas ${formatPrice(price)} por mês`}
+            <Button
+              key={price.id}
+              onClick={() => {}}
+              disabled={isLoading || price.id === priceIdLoading}
+              className="mb-4"
+            >
+              {`Seja usuário Premium por apenas ${formatPrice(price)}/mês`}
             </Button>
           ))
         })}
