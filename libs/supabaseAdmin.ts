c397544e5f67a26a4b0092a1b1e312af1675fd82
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-08-08 10:52:41 -0300

    update lib

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -5,6 +5,7 @@ import { Database } from '@/types_db'
 import { Price, Product } from '@/types'
 
 import { stripe } from './stripe'
+import { toDateTime } from './helpers'
 
 export const supabaseAdmin = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
@@ -50,7 +51,7 @@ const upsertPriceRecord = async (price: Stripe.Price) => {
   console.log(`Preço inserido/atualizado: ${price.id}`)
 }
 
-const createOrRetrieveACustomer = async ({
+const createOrRetrieveCustomer = async ({
   email,
   uuid,
 }: {
@@ -86,4 +87,107 @@ const createOrRetrieveACustomer = async ({
   }
 
   return data.stripe_customer_id
+}
+
+const copyBillingDetailsToCustomer = async (
+  uuid: string,
+  payment_method: Stripe.PaymentMethod
+) => {
+  const customer = payment_method.customer as string
+  const { name, phone, address } = payment_method.billing_details
+
+  if (!name || !phone || !address) return
+
+  await stripe.customers.update(customer, { name, phone, address })
+  const { error } = await supabaseAdmin
+    .from('users')
+    .update({
+      billing_address: { ...address },
+      payment_method: { ...payment_method[payment_method.type] },
+    })
+    .eq('id', uuid)
+
+  if (error) throw error
+}
+
+const manageSubscriptionStatusChange = async (
+  subscriptionId: string,
+  customerId: string,
+  createAction = false
+) => {
+  // Get customer's UUID from mapping table.
+  const { data: customerData, error: noCustomerError } = await supabaseAdmin
+    .from('customers')
+    .select('id')
+    .eq('stripe_customer_id', customerId)
+    .single()
+
+  if (noCustomerError) throw noCustomerError
+
+  const { id: uuid } = customerData!
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
+    expand: ['default_payment_method'],
+  })
+  // Upsert the latest status of the subscription object.
+  const subscriptionData: Database['public']['Tables']['subscriptions']['Insert'] =
+    {
+      id: subscription.id,
+      user_id: uuid,
+      metadata: subscription.metadata,
+      // @ts-ignore
+      status: subscription.status,
+      price_id: subscription.items.data[0].price.id,
+      //TODO check quantity on subscription
+      // @ts-ignore
+      quantity: subscription.quantity,
+      cancel_at_period_end: subscription.cancel_at_period_end,
+      cancel_at: subscription.cancel_at
+        ? toDateTime(subscription.cancel_at).toISOString()
+        : null,
+      canceled_at: subscription.canceled_at
+        ? toDateTime(subscription.canceled_at).toISOString()
+        : null,
+      current_period_start: toDateTime(
+        subscription.current_period_start
+      ).toISOString(),
+      current_period_end: toDateTime(
+        subscription.current_period_end
+      ).toISOString(),
+      created: toDateTime(subscription.created).toISOString(),
+      ended_at: subscription.ended_at
+        ? toDateTime(subscription.ended_at).toISOString()
+        : null,
+      trial_start: subscription.trial_start
+        ? toDateTime(subscription.trial_start).toISOString()
+        : null,
+      trial_end: subscription.trial_end
+        ? toDateTime(subscription.trial_end).toISOString()
+        : null,
+    }
+
+  const { error } = await supabaseAdmin
+    .from('subscriptions')
+    .upsert([subscriptionData])
+
+  if (error) throw error
+
+  console.log(
+    `Subscription criada/atualizada [${subscription.id}] para o usuário [${uuid}]`
+  )
+
+  // For a new subscription copy the billing details to the customer object.
+  // NOTE: This is a costly operation and should happen at the very end.
+  if (createAction && subscription.default_payment_method && uuid)
+    //@ts-ignore
+    await copyBillingDetailsToCustomer(
+      uuid,
+      subscription.default_payment_method as Stripe.PaymentMethod
+    )
+}
+
+export {
+  upsertProductRecord,
+  upsertPriceRecord,
+  createOrRetrieveCustomer,
+  manageSubscriptionStatusChange,
 }
